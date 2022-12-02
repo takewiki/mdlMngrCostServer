294f588716710d5a8d71952507e3625604584c90
takewiki <hulilei@takewiki.com.cn>
2022-12-02 20:13:27 +0800

    v1.1

--- R/mfgCost.R
+++ R/mfgCost.R
@@ -10,8 +10,8 @@
 #' @export
 #'
 #' @examples
-#' mngrCost_Read()
-mngrCost_Read <- function(file_name = "data-raw/产品管理成本模板.xlsx",
+#' mfgCost_Read()
+mfgCost_Read <- function(file_name = "data-raw/产品生产成本模板.xlsx",
                           sheet = "data",
                           FYear=2022,
                           FPeriod=9,
@@ -32,7 +32,7 @@ mngrCost_Read <- function(file_name = "data-raw/产品管理成本模板.xlsx",
                      'FPCSPerPkg',
                      'FMiddleSmallCoefficent',
                      'FBigMiddleCoefficent',
-                     'FMngrCost',
+                     'FMfgCost',
                      'FItemProperty',
                      'FGroupName',
                      'FBaseUnit',
@@ -45,9 +45,9 @@ mngrCost_Read <- function(file_name = "data-raw/产品管理成本模板.xlsx",
     data$Fyear <-FYear
     data$Fperiod <- FPeriod
     #删除已经数据
-    mngrCost_delete(dms_token = dms_token,FYear=FYear,FPeriod = FPeriod)
+    mfgCost_delete(dms_token = dms_token,FYear=FYear,FPeriod = FPeriod)
     #上传数据
-    tsda::db_writeTable2(token = dms_token,table_name = 'rds_bd_mngrCost',r_object = data,append = T)
+    tsda::db_writeTable2(token = dms_token,table_name = 'rds_bd_mfgCost',r_object = data,append = T)
   }
 
   return(data)
@@ -66,10 +66,10 @@ mngrCost_Read <- function(file_name = "data-raw/产品管理成本模板.xlsx",
 #' @export
 #'
 #' @examples
-#' mngrCost_delete()
-mngrCost_delete <-function(dms_token ='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',FYear=2022,FPeriod=9){
+#' mfgCost_delete()
+mfgCost_delete <-function(dms_token ='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',FYear=2022,FPeriod=9){
 
-  sql <- paste0("delete  FROM [dbo].[rds_bd_mngrCost]
+  sql <- paste0("delete  FROM [dbo].[rds_bd_mfgCost]
   where fyear= ",FYear," and fperiod = ",FPeriod,"")
   tsda::sql_update2(token = dms_token,sql_str = sql)
   res <- TRUE
@@ -92,8 +92,8 @@ mngrCost_delete <-function(dms_token ='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',FYe
 #' @export
 #'
 #' @examples
-#' mngrCost_SyncLastOne()
-mngrCost_SyncLastOne <- function(dms_token ='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',FYear=2022,FPeriod=9
+#' mfgCost_SyncLastOne()
+mfgCost_SyncLastOne <- function(dms_token ='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',FYear=2022,FPeriod=9
 ) {
   #获取上一年信息
   if(FPeriod==1){
@@ -105,9 +105,9 @@ mngrCost_SyncLastOne <- function(dms_token ='9B6F803F-9D37-41A2-BDA0-70A7179AF0F
 
   }
   #删除已有数据
-  mngrCost_delete(dms_token = dms_token,FYear =FYear ,FPeriod =FPeriod )
+  mfgCost_delete(dms_token = dms_token,FYear =FYear ,FPeriod =FPeriod )
 
-  sql <- paste0("insert into rds_bd_mngrCost
+  sql <- paste0("insert into rds_bd_mfgCost
 SELECT [FNumber]
       ,[Fname]
       ,[Fmodel]
@@ -115,7 +115,7 @@ SELECT [FNumber]
       ,[FPCSPerPkg]
       ,[FMiddleSmallCoefficent]
       ,[FBigMiddleCoefficent]
-      ,[FMngrCost]
+      ,[FMfgCost]
       ,[FItemProperty]
       ,[FGroupName]
       ,[FBaseUnit]
@@ -124,7 +124,7 @@ SELECT [FNumber]
       ,[FPrdCategory]
       ,",FYear," as [Fyear]
       ,",FPeriod," as  [Fperiod]
-  FROM [dbo].[rds_bd_mngrCost]
+  FROM [dbo].[rds_bd_mfgCost]
   where fyear= ",FYearLast," and fperiod = ",FPeriodLast,"")
   tsda::sql_update2(token = dms_token,sql_str = sql)
   res <- TRUE
@@ -145,9 +145,9 @@ SELECT [FNumber]
 #' @export
 #'
 #' @examples
-#' mngrCost_query()
+#' mfgCost_query()
 #'
-mngrCost_query <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',
+mfgCost_query <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',
                            FYear=2022,
                            FPeriod=8){
   sql <- paste0("SELECT  [FNumber] as 编码
@@ -157,7 +157,7 @@ mngrCost_query <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',
       ,[FPCSPerPkg] as 单箱标准数量
       ,[FMiddleSmallCoefficent] as 中小包装换算比
       ,[FBigMiddleCoefficent] as 大中包装换算比
-      ,[FMngrCost] as  管理成本
+      ,[FMfgCost] as  生产成本
       ,[FItemProperty] as 物料属性
       ,[FGroupName] as 物料分组
       ,[FBaseUnit] as 基本单位
@@ -166,7 +166,7 @@ mngrCost_query <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',
       ,[FPrdCategory] as 产品大类
       ,[Fyear] as 年份
       ,[Fperiod] as 月份
-  FROM  [rds_bd_mngrCost]
+  FROM  [rds_bd_mfgCost]
   where  fyear = ",FYear,"  and fperiod = ",FPeriod," ")
   data = tsda::sql_select2(token = dms_token,sql = sql)
   return(data)
@@ -186,13 +186,13 @@ mngrCost_query <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',
 #' @export
 #'
 #' @examples
-#' mngrCost_query()
+#' mfgCost_query()
 #'
-mngrCost_checkExists <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',
+mfgCost_checkExists <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3',
                            FYear=2022,
                            FPeriod=8){
   sql <- paste0("SELECT   1
-  FROM  [rds_bd_mngrCost]
+  FROM  [rds_bd_mfgCost]
   where  fyear = ",FYear,"  and fperiod = ",FPeriod," ")
   data = tsda::sql_select2(token = dms_token,sql = sql)
   ncount =nrow(data)
@@ -219,47 +219,47 @@ mngrCost_checkExists <- function(dms_token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3
 #' @export
 #'
 #' @examples
-#' mngrCost_Server()
-mngrCost_Server <- function(input,output,session,dms_token) {
-  #查询管理成本
-  var_txt_mngrCost_year <- tsui::var_text('txt_mngrCost_year')
-  var_txt_mngrCost_period <- tsui::var_text('txt_mngrCost_period')
+#' mfgCost_Server()
+mfgCost_Server <- function(input,output,session,dms_token) {
+  #查询生产成本
+  var_txt_mfgCost_year <- tsui::var_text('txt_mfgCost_year')
+  var_txt_mfgCost_period <- tsui::var_text('txt_mfgCost_period')
 
-  shiny::observeEvent(input$btnmngrCost_query,{
+  shiny::observeEvent(input$btnmfgCost_query,{
     print('bug')
 
-    FYear = as.integer(var_txt_mngrCost_year())
-    FPeriod = as.integer(var_txt_mngrCost_period())
-    data = mngrCost_query(dms_token = dms_token,FYear =FYear ,FPeriod = FPeriod)
-    tsui::run_dataTable2(id = 'dataviewmngrCost_query',data=data)
-    file_name =paste0('产品管理成本',FYear,'_',FPeriod,'.xlsx')
-    tsui::run_download_xlsx(id = 'btnmngrCost_dl',data = data,filename = file_name)
+    FYear = as.integer(var_txt_mfgCost_year())
+    FPeriod = as.integer(var_txt_mfgCost_period())
+    data = mfgCost_query(dms_token = dms_token,FYear =FYear ,FPeriod = FPeriod)
+    tsui::run_dataTable2(id = 'dataviewmfgCost_query',data=data)
+    file_name =paste0('产品生产成本',FYear,'_',FPeriod,'.xlsx')
+    tsui::run_download_xlsx(id = 'btnmfgCost_dl',data = data,filename = file_name)
 
 
 
   })
 
-  #上传管理成本
-  var_filemngrCost_upload <- tsui::var_file('filemngrCost_upload')
-  var_txt_mngrCost_sheet <- tsui::var_text('txt_mngrCost_sheet')
-  shiny::observeEvent(input$btnmngrCost_upload,{
-    file_name = var_filemngrCost_upload()
-    sheet_name = var_txt_mngrCost_sheet()
-    FYear = as.integer(var_txt_mngrCost_year())
-    FPeriod = as.integer(var_txt_mngrCost_period())
-    mdlMngrCostServer::mngrCost_Read(file_name = file_name,sheet = sheet_name,FYear = FYear ,FPeriod = FPeriod,dms_token = dms_token)
+  #上传生产成本
+  var_filemfgCost_upload <- tsui::var_file('filemfgCost_upload')
+  var_txt_mfgCost_sheet <- tsui::var_text('txt_mfgCost_sheet')
+  shiny::observeEvent(input$btnmfgCost_upload,{
+    file_name = var_filemfgCost_upload()
+    sheet_name = var_txt_mfgCost_sheet()
+    FYear = as.integer(var_txt_mfgCost_year())
+    FPeriod = as.integer(var_txt_mfgCost_period())
+    mdlmfgCostServer::mfgCost_Read(file_name = file_name,sheet = sheet_name,FYear = FYear ,FPeriod = FPeriod,dms_token = dms_token)
 
-    tsui::pop_notice('产品管理成本上传成功')
+    tsui::pop_notice('产品生产成本上传成功')
 
 
   })
 
-  #同步上月管理成本
-  shiny::observeEvent(input$btnmngrCost_syncLastOne,{
-    FYear = as.integer(var_txt_mngrCost_year())
-    FPeriod = as.integer(var_txt_mngrCost_period())
-    mngrCost_SyncLastOne(dms_token = dms_token,FYear =FYear ,FPeriod = FPeriod)
-    tsui::pop_notice('产品管理成本同步成功')
+  #同步上月生产成本
+  shiny::observeEvent(input$btnmfgCost_syncLastOne,{
+    FYear = as.integer(var_txt_mfgCost_year())
+    FPeriod = as.integer(var_txt_mfgCost_period())
+    mfgCost_SyncLastOne(dms_token = dms_token,FYear =FYear ,FPeriod = FPeriod)
+    tsui::pop_notice('产品生产成本同步成功')
 
 
 
